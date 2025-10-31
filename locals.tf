92c18a1e17699fad696e1029166e5b0716430f4c
shuangyi-jessica <65968101+shuangyi-jessica@users.noreply.github.com>
2025-10-31 14:52:16 +0100

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,8 +1,8 @@
 locals {
-  terraform_cloud_workspace_name      = "user-${var.subscription_name}"
-  user_assigned_managed_identity_name = "umi-${var.subscription_name}"
+  terraform_cloud_workspace_name      = "user-sub-vending"
+  user_assigned_managed_identity_name = "umi-sub-vending"
   identity_resource_group_name        = "rg-identity"
-  github_repository_name              = var.subscription_name
+  github_repository_name              = "sub-vending"
 }
 
 locals {
